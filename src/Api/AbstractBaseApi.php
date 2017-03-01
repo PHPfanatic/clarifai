52dedb29d87b727b21c86415609d220b7e7deaa2
Nick <git@phpfanatic.com>
2017-03-01 03:14:07 -0500

    Restructured project layout, added GenerateToken.

--- src/Api/AbstractBaseApi.php
+++ src/Api/AbstractBaseApi.php
@@ -1,5 +1,14 @@
 <?php namespace PhpFanatic\clarifAI\Api;
 
+/**
+ * Base API functionality for clarifAI.  This abstract class is a bit heavy, but handles all of the communication
+ * between the extended client class.
+ *
+ * @author   Nick White <git@phpfanatic.com>
+ * @link     https://github.com/PHPfanatic/clarifAI
+ * @version  0.1.1
+ */
+
 abstract class AbstractBaseApi implements AuthInterface
 {
 	private $clientid = null;
@@ -41,11 +50,39 @@ abstract class AbstractBaseApi implements AuthInterface
 	public function SetApiUrl() {
 		$this->apiurl = $this->endpoint . '/' . $this->version;
 	}
-		
+	
+	/**
+	 * Generate OAUTH token and set the access variable as needed.
+	 * {@inheritDoc}
+	 * @see \PhpFanatic\clarifAI\Api\AuthInterface::GenerateToken()
+	 * @throws \ErrorException
+	 * @return null
+	 */
 	public function GenerateToken() {
-		$this->access['token'] = '';
-		$this->access['token_time'] = '';
-		$this->access['token_expires'] = '';
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_HEADER, array('Content-Type: application/json'));
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($ch, CURLOPT_USERPWD, $this->clientid . ":" . $this->clientsecret);
+		curl_setopt($ch, CURLOPT_URL, $this->apiurl . '/token');
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		
+		$result = curl_exec($ch);
+		
+		//The header is returned along with the response, we parse it out here.
+		$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+		$header = substr($result, 0, $header_size);
+		$result = json_decode(substr($result, $header_size), true);
+		
+		curl_close($ch);
+
+		if($result['status']['code'] == '10000'){
+			$this->access['token'] = $result['access_token'];
+			$this->access['token_time'] = (int)date('U');
+			$this->access['token_expires'] = $result['expires_in'];
+		}
+		else {
+			throw new \ErrorException('Token generation failed.');
+		}
 	}
 	
 	public function IsTokenValid() {
@@ -53,7 +90,7 @@ abstract class AbstractBaseApi implements AuthInterface
 			return false;
 		}
 	
-		if((date('U') - $this->access['token_time']) < $this->access['token_expires']) {
+		if(((int) date('U') - $this->access['token_time']) < $this->access['token_expires']) {
 			return false;
 		}
 		
--- src/Client.php
+++ src/Client.php
@@ -1,12 +1,9 @@
 <?php namespace PhpFanatic\clarifAI;
 
 use PhpFanatic\clarifAI\Api\AbstractBaseApi;
-use PhpFanatic\clarifAI\Api\PackageTrait;
 
 class Client extends AbstractBaseApi 
-{
-	use PackageTrait;
-	
+{	
 	public function __construct($clientid, $clientsecret) {
 		parent::__construct($clientid, $clientsecret);
 	}
